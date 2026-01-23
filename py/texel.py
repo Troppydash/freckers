63a219fb754fe5dadca062c1bfd24150e98d3bb8
Troppydash <troppydash.developer@gmail.com>
2026-01-24 04:22:05 +1300

    some fixes

---
+++ py/texel.py
@@ -0,0 +1,84 @@
+# file to generate the texel tuning dataset
+import glob
+import math
+import os
+import pickle
+import random
+
+pk_file = "session6"
+session = "session6"
+
+
+def bitmask_to_array(mask: int):
+    arr = []
+    for i in range(64):
+        if (mask & (1 << i)) > 0:
+            arr.append(1)
+        else:
+            arr.append(0)
+
+    return arr
+
+
+def generator():
+    xs = []
+    ys = []
+    for file in glob.glob(f'./sessions/{pk_file}/*.pk'):
+        if file.endswith('_backup.pk'):
+            continue
+
+        print(f'[dataset] loading {os.path.basename(file)}')
+
+        try:
+            with open(file, 'rb') as f:
+                dataset = pickle.load(f)
+        except Exception as e:
+            print(f'skipping: {e}')
+            continue
+
+        for i in range(len(dataset.positions)):
+            # 9mllion, requires 80k,
+            prob = 80 / 9000
+            if not (random.random() < prob):
+                continue
+
+            positions = dataset.positions[i]
+            outcome = dataset.outcomes[i]
+            lily = bitmask_to_array(positions[0])
+            red = bitmask_to_array(positions[1])
+            blue = list(reversed(bitmask_to_array(positions[2])))
+            turn = positions[3]
+            moves = positions[4]
+            eval = dataset.evals[i]
+
+            xs.append([positions[0], positions[1], positions[2], positions[3]])
+
+            if outcome == turn:
+                score = 1
+            elif outcome == 1 - turn:
+                score = -1
+            else:
+                score = 0
+
+            # our score is in the perspective of the moving player
+
+            lambda_ = 0.85
+            if eval > 100000:
+                normalized = 1
+            elif eval < -100000:
+                normalized = -1
+            else:
+                normalized = 2 / (1 + math.exp(-eval / 1000)) - 1
+            avg = lambda_ * score + (1 - lambda_) * normalized
+            ys.append([avg])
+
+    # write to file
+    text = []
+    for x, y in zip(xs, ys):
+        text.append(f"{x[0]} {x[1]} {x[2]} {x[3]} {y[0]}")
+
+    with open('texel.txt', 'w') as f:
+        f.write('\n'.join(text))
+
+if __name__ == '__main__':
+    generator()
